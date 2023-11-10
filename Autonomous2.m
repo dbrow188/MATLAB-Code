a576e56673a201bdf33444609a3956d54480ccf1
dbrow188 <147113192+dbrow188@users.noreply.github.com>
2023-11-09 23:25:09 -0700

    Add files via upload

--- Autonomous2.m
+++ Autonomous2.m
@@ -5,70 +5,103 @@ x = 1;
 brick.GyroCalibrate(1);
 target = 0;
 
+brickPress = brick.TouchPressed(4);
+
+brick.SetColorMode(3, 2);
 
 
 while x == 1
-    disp("Target 1");
-    %{
-    if brick.UltrasonicDist(2) == 255
-       brick.StopMotor('A', 'Brake')
-       brick.StopMotor('D', 'Brake')
-       pause(1)
-       brick.MoveMotorAngleRel('A', 70, 40, 'Brake');
-       brick.MoveMotorAngleRel('D', 70, -40, 'Brake');
-    end
-    %}
-
-
-     if brick.UltrasonicDist(2) < 255
+    disp("Target 1")
+    disp(brick.UltrasonicDist(2));
+     if brick.UltrasonicDist(2) <= 35 && brick.UltrasonicDist(2) > 0
         disp("Target 2");
-        brick.MoveMotorAngleRel('AD', 40, 100, 'Coast');
-     end
-     if brick.TouchPressed(4) == 1
-
-            disp("Target 3");
-            disp(brick.TouchPressed(4));
-            target = brick.GyroAngle(1) - 90;
-            brick.StopAllMotors('Brake');
-            brick.MoveMotorAngleRel('AD', 100, -360, 'Brake');
-            pause(3);
-                while brick.GyroAngle(1) >= target
-                disp(brick.GyroAngle(1));
-                brick.MoveMotorAngleRel('A', 30, 30,'Coast');
-                brick.MoveMotorAngleRel('D', 30, -30, 'Coast');
-                end
-     end 
+        brick.MoveMotorAngleRel('AD', 40, 240, 'Brake');
         
-     %{
-     elseif brick.UltrasonicDist(2) <= 20 && brick.UltrasonicDist(2) >= 19
-        brick.MoveMotorAngleRel('AD', 50, 100, 'Coast')
+     elseif brick.UltrasonicDist(2) > 35 && brick.UltrasonicDist(2) < 256
+        target = target + 90;
+        brick.MoveMotorAngleRel('AD', 100, 360, 'Coast')
         pause(1);
-    elseif brick.UltrasonicDist(2) > 20
-        brick.StopAllMotors('Coast')
-        brick.MoveMotorAngleRel('A', 30, -10, 'Coast')
-        brick.MoveMotorAngleRel('D', 30, 40, 'Coast')
-       
-    elseif brick.UltrasonicDist(2) < 19
-        brick.StopAllMotors('Coast')
-        brick.MoveMotorAngleRel('A', 40, 10, 'Coast')
-        brick.MoveMotorAngleRel('D', 30,-10, 'Coast')
-        brick.MoveMotorAngleRel('AD', 40,)
-        
-    end 
-    disp(brick.TouchPressed(4));
-     %}
-     %{
-    if brick.TouchPressed(4) == 1
+            while brick.GyroAngle(1) <= target
+            disp(brick.GyroAngle(1));
+            brick.MoveMotorAngleRel('A', 40, -20,'Coast');
+            brick.MoveMotorAngleRel('D', 40, 20, 'Coast');
+            end
+            while brick.GyroAngle(1) >= target
+            disp(brick.GyroAngle(1));
+            brick.MoveMotorAngleRel('A', 20, 10, 'Coast');
+            end
+            pause(1);
+            while brick.UltrasonicDist(2) > 25
+                brick.MoveMotorAngleRel('AD', 20, 100, 'Coast')
+                pause(0.5)
+            end
+            brick.MoveMotorAngleRel('AD', 30, 180, 'Coast');
+            pause(1);
+     end
+
+
+     disp(brick.TouchPressed(4));
+     brickPress = brick.TouchPressed(4);
+     if brickPress == 1
+        brickPress = 0;
+        disp("Target 3");
+        disp(brick.TouchPressed(4));
+        target = target - 90;
         brick.StopAllMotors('Brake');
         brick.MoveMotorAngleRel('AD', 100, -360, 'Brake');
-        pause(2)
-        brick.MoveMotorAngleRel('A', 100, 700, 'Brake');
-        brick.MoveMotorAngleRel('D', 100, -700, 'Brake');
-        pause(3)
+        pause(3);
+            while brick.GyroAngle(1) >= target
+                disp(brick.GyroAngle(1));
+                brick.MoveMotorAngleRel('A', 40, 20,'Coast');
+                brick.MoveMotorAngleRel('D', 40, -20, 'Coast');
+            end
+            while brick.GyroAngle(1) <= target
+                    disp(brick.GyroAngle(1));
+                    brick.MoveMotorAngleRel('D', 20, 20,'Brake')
+            end
+            pause(1);
+     end 
+
+        if brick.ColorCode(3) == 5  
+            brick.StopAllMotors('Brake');
+            pause(1);
+            brick.MoveMotorAngleRel('AD', 100, 360, 'Coast')
+    
+        elseif brick.ColorCode(3) == 4
+            brick.StopAllMotors('Brake');
         
-    end 
-    disp(brick.TouchPressed(4))
-     %}
+            disp("It should beep");
+            for i = 0: 1: 3    
+                brick.beep();
+                pause(0.5);
+                disp(i);
+            end
+            brick.MoveMotorAngleRel('AD', 100, 360, 'Coast')
+
+        elseif brick.ColorCode(3) == 3
+            brick.StopAllMotors('Brake');
+        
+            disp("It should beep");
+            for i = 0: 1: 2    
+            brick.beep();
+            pause(0.5);
+            disp(i);
+            end
+            brick.MoveMotorAngleRel('AD', 100, 360, 'Coast')
+    
+        elseif brick.ColorCode(3) == 2
+            brick.StopAllMotors('Brake');
+        
+            disp("It should beep");
+            for i = 0: 1: 1  
+                brick.beep();
+            pause(0.5);
+            disp(i);
+            end
+            brick.MoveMotorAngleRel('AD', 100, 360, 'Coast')
+        end
+
+
 switch key
     case 'w'
         x = 2;
@@ -77,7 +110,42 @@ switch key
     case 'e'
         brick.StopAllMotors('Brake');
 end
-
 end
 
+while x == 0
+    pause(0.001);
+    switch key
+        case 'uparrow'
+            brick.MoveMotorAngleRel('AD', 100, 60, 'Brake');
+            
+
+        case 'downarrow'
+            brick.MoveMotorAngleRel('A', 100, -40, 'Brake');
+            brick.MoveMotorAngleRel('D', 100, -40, 'Brake');
+            
+        case 'leftarrow'
+            brick.MoveMotorAngleRel('A', 70, 40, 'Brake');
+            brick.MoveMotorAngleRel('D', 70, -40, 'Brake');
+           
+
+        case 'rightarrow'
+            brick.MoveMotorAngleRel('A', 70, -40, 'Brake');
+            brick.MoveMotorAngleRel('D', 70, 40, 'Brake');
+
+
+        case 'z'
+            brick.MoveMotorAngleRel('B', 100, -110, 'Brake');
+
+        case 'x'
+            brick.MoveMotorAngleRel('B', 100, 110, 'Brake');
+
+
+
+
+        case 'q'
+
+            break;
+    end
+end
+CloseKeyboard();
     
